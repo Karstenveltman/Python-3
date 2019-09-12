7be33f7965f96f07fe40420f31e79967f8a71eb4
Karsten Veltman <karsten.veltman@coderclass.nl>
2019-09-12 09:53:50 +0200

    kan nu bestanden lezen

--- python/eindopdracht.py
+++ python/eindopdracht.py
@@ -1,17 +1,51 @@
-def menu():
-    print("dit is een test om te kijken welk cijfer meer gekozen word\nkies 1 of 2 asjeblieft")
-    menu.keuze = input("wat kies je: ")
+def readfile():
+    with open("filenaam.txt") as f:
+      bestandsdata = f.read().split('\n')
 
+    dictionary = {}
+
+    for item in bestandsdata:
+        if not item == '':
+            woord1, woord2 = item.split("=")
+            dictionary[woord1] = woord2
+    return dictionary
+
+print(readfile())
+
+def menu():
+    print("Dit is een test om te kijken wat populairder is, kat of hond.")
+    menu.naam = str(input("voer hier je naam in: "))
 
 menu()
 
-def keuzeverwerken():
-    if str.isdigit(menu.keuze) == True:
-        if menu.keuze == "1":
-            print("je hebt functie 1 gekozen")
-        elif menu.keuze == "2":
-            print("je hebt functie 2 gekozen")
-    else:
-        print("kies 1 of 2")
+def verwerken():
+    allenamen[menu.naam] = ""
 
-keuzeverwerken()
+verwerken()
+
+def keuze():
+    kies = str(input("\nkies kat of hond: "))
+    if kies == "hond":
+        print("minder goeie keuze")
+        return "hond"
+    elif kies == "kat":
+        print("goeie keuze")
+        return "kat"
+    else:
+        keuze()
+
+
+
+a = keuze()    
+if a == "hond":
+    allenamen[menu.naam] = "hond"
+    
+elif a == "kat":
+    allenamen[menu.naam] = "kat"
+
+
+
+    
+
+
+
